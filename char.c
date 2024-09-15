488c38169457d85c3c767bd2d2bf62f4943cca5a
Isha Negi <itsishanegi@gmail.com>
2024-09-15 20:36:33 +0530

    Add files via upload

---
+++ char.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+#define MAX 10
+
+void main()
+
+{ char a[MAX]= {'a','d','f'};
+   
+printf("size of char:  %lu \n",sizeof(char));
+   
+printf("size of array a:  %lu \n",sizeof(a));
+   
+printf("the characters are:\n");
+   
+for(int i=0;i<MAX;i++)
+{
+       
+printf("%d.%c\n",i+1,a[i]);
+   
+}
+
+}
